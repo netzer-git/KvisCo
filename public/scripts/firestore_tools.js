31caa596b2068e86a6298876f8235dd9ee26f71a
Netzer <Netzerep@gmail.com>
2021-06-13 21:30:07 +0300

    added geocode function

--- public/scripts/firestore_tools.js
+++ public/scripts/firestore_tools.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true });
+db.settings({
+    timestampsInSnapshots: true
+});
 const storage = firebase.storage();
 
 
 /*
-* the function takes docID and collection name and resolve a promise of the document.
-* the function does not return the doc, it returns the promise.
-* USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
-*/
+ * the function takes docID and collection name and resolve a promise of the document.
+ * the function does not return the doc, it returns the promise.
+ * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
+ */
 function promiseLoaderByCollectionAndId(collection, documentID) {
     return new Promise((resolve, reject) => {
         var query = db.collection(collection).doc(documentID);
-        
+
         query.get().then((doc) => {
             if (doc.exists) {
                 console.log("Document data: ", doc.data());
@@ -30,19 +32,19 @@ function promiseLoaderByCollectionAndId(collection, documentID) {
 }
 
 /*
-* the function takes docID - the id of the washer - and resolve a promise of the document of the washer.
-* the function does not return the doc, it returns the promise.
-* USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
-*/
+ * the function takes docID - the id of the washer - and resolve a promise of the document of the washer.
+ * the function does not return the doc, it returns the promise.
+ * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
+ */
 function promiseWasherLoaderById(documentID) {
     return promiseLoaderByCollectionAndId('washers', documentID);
 }
 
 /*
-* the function takes docID - the id of the user - and resolve a promise of the document of the user.
-* the function does not return the doc, it returns the promise.
-* USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
-*/
+ * the function takes docID - the id of the user - and resolve a promise of the document of the user.
+ * the function does not return the doc, it returns the promise.
+ * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
+ */
 function promiseUserLoaderById(documentID) {
     return promiseLoaderByCollectionAndId('users', documentID);
 
@@ -50,40 +52,46 @@ function promiseUserLoaderById(documentID) {
 
 
 /*
-* the function resolve a promise of the document of the current user based on the Auth system.
-* the function does not return the doc, it returns the promise.
-* USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
-*/
+ * the function resolve a promise of the document of the current user based on the Auth system.
+ * the function does not return the doc, it returns the promise.
+ * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
+ */
 function promiseUserLoaderByCurrentUserID() {
     console.log(getUserToken());
     return promiseLoaderByCollectionAndId('users', getUserToken());
 }
 
 /*
-* the function resolve a promise of the document of the current washer based on the Auth system.
-* the function does not return the doc, it returns the promise.
-* USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
-*/
+ * the function resolve a promise of the document of the current washer based on the Auth system.
+ * the function does not return the doc, it returns the promise.
+ * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
+ */
 function promiseWasherLoaderByCurrentUserID() {
     return promiseLoaderByCollectionAndId('washers', getUserToken());
 }
 
 /*
-* the function takes doc from washers collection (the resolve of a promise!), and gets the washer rating
-*/
+ * the function takes doc from washers collection (the resolve of a promise!), and gets the washer rating
+ */
 function getRatingFromDoc(doc) {
     return doc.data().rating_sum / doc.data().rating_num;
 }
 
 /*
-* the function takes washerID and resolves a promise of multiple orders (of the current washer) by specific given status
-* USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
-*/
+ * the function takes washerID and resolves a promise of multiple orders (of the current washer) by specific given status
+ * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
+ */
 function promiseOrderArrayByWasherIdAndStatus(washerID, status) {
     return new Promise((resolve, reject) => {
         // to look for doc-ref field, you have to get the ref
         const washerRef = db.collection("washers").doc(washerID);
-        var query = db.collection('orders').where("washer", "==", washerRef).where('status', '==', status).orderBy("created_at");
+        if (status === "all") {
+            var query = db.collection('orders').where("washer", "==", washerRef).orderBy("created_at");
+        } else if (status === "processing") {
+            var query = db.collection('orders').where("washer", "==", washerRef).where('status', '!=', "finished").orderBy("created_at");
+        } else {
+            var query = db.collection('orders').where("washer", "==", washerRef).where('status', '==', status).orderBy("created_at");
+        }
 
         query.get().then((docArray) => {
             const orderArray = [];
@@ -120,26 +128,24 @@ function saveImageToUser(file) {
                 storageUrl: fileSnapshot.metadata.fullPath
             })
         });
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.error('There was an error uploading a file to Cloud Storage:', error);
-      });
-  }
+    });
+}
 
 function onMediaFileSelected(event) {
-  event.preventDefault();
-  var file = event.target.files[0];
+    event.preventDefault();
+    var file = event.target.files[0];
 
-  // Clear the selection in the file picker input.
-  imageFormElement.reset();
+    // Clear the selection in the file picker input.
+    imageFormElement.reset();
 
-  // Check if the file is an image.
-  if (!file.type.match('image.*')) {
-    console.log("That's not an image");
-  }
-  else if (isUserSignedIn()) {
-    saveImageToUser(file);
-  }
-  else {
-    console.log("You are not connected");
-  }
+    // Check if the file is an image.
+    if (!file.type.match('image.*')) {
+        console.log("That's not an image");
+    } else if (isUserSignedIn()) {
+        saveImageToUser(file);
+    } else {
+        console.log("You are not connected");
+    }
 }
