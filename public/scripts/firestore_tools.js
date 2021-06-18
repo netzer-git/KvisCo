cf510ecc0f5917cf16a47d2fd2142d4e5b25d606
Netzer <Netzerep@gmail.com>
2021-06-18 14:37:49 +0300

    mid-marathon changes

--- public/scripts/firestore_tools.js
+++ public/scripts/firestore_tools.js
@@ -129,24 +129,24 @@ function promiseOrderArrayByFieldIdAndStatus(field, docID, status) {
  * the function takes washerID and resolves a promise of multiple orders (of the current washer) by specific given status
  * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
  */
-function promiseOrderArrayByWasherIdAndStatus(washerID, status) {
-    return promiseOrderArrayByFieldIdAndStatus("washer", washerID, status);
+function promiseOrderArrayByWasherIdAndStatus(washerId, status) {
+    return promiseOrderArrayByFieldIdAndStatus("washer", washerId, status);
 }
 
 /*
  * the function takes userID and resolves a promise of multiple orders (of the current washer) by specific given status
  * USAGE: promiseWasherLoaderById(docID).then(doc => { // do something with.doc.data })
  */
-function promiseOrderArrayByUserIdAndStatus(userID, status) {
-    return promiseOrderArrayByFieldIdAndStatus("user", userID, status);
+function promiseOrderArrayByUserIdAndStatus(userId, status) {
+    return promiseOrderArrayByFieldIdAndStatus("user", userId, status);
 }
 
 /**
  * creates new order from order object and saves it to firestore server.
  * @param {*} order : basic order object
  */
-function createNewOrder(order) {
-    db.collection("orders").add({
+async function createNewOrder(order) {
+    let newOrderRef = await db.collection("orders").add({
         user: db.doc('users/' + order.userID),
         washer: db.doc('washers/' + order.washerID),
         due_to: order.due_to,
@@ -160,16 +160,37 @@ function createNewOrder(order) {
         review_user: null,
         properties: order.properties,
         laundry_pic: null,
-        comments: null,
-    }).then((docRef) => {
-        console.log("New order added: " + docRef);
-    })
+        comments: order.comments,
+    });
+    return newOrderRef.id;
+}
+
+/**
+ * update the details of specific order
+ * @param {*} orderDetails order details dict
+ * @param {*} orderId order id
+ */
+ async function setOrderDetails(orderDetails, orderId) {
+    let washer = await db.collection('orders').doc(orderId);
+    await washer.update(orderDetails).catch((err) => {
+        console.error("error updating order details: " + err);
+    });
+}
+
+/**
+ * update the open times of specific washer
+ * @param {*} openTimes dict of opening times
+ * @param {*} washerId washer id
+ */
+async function setWasherOpenTimes(openTimes, washerId) {
+    let washer = await db.collection('washers').doc(washerId);
+    await washer.update({ opening_time : openTimes});
 }
 
 /**
  * create new user from the current user (on auth) and basic user object
  * please notice: the user needs to be signed in (in auth) while creating new user (on firestore)
- * @param {object} user : basic user object
+ * @param {object} user : basic user object (from auth)
  */
 function createNewUser(user) {
     db.collection("users").doc(getUserToken()).set({
@@ -185,22 +206,76 @@ function createNewUser(user) {
     })
 }
 
-
-// Saves a new message containing an image in Firebase.
-// This first saves the image in Firebase storage.
-function saveImageToUser(file) {
-
+/**
+ * Saves a new message containing an image in Firebase. This first saves the image in Firebase storage.
+ * @param {*} file image file
+ * @return {string} image url path to firebase storage
+ */
+async function saveImageToUser(file) {
     let filePath = getUserToken() + '/' + file.name;
-
-    storage.ref(filePath).put(file).then((fileSnapshot) => {
-        fileSnapshot.ref.getDownloadURL().then((url) => {
-            db.collection('users').doc(getUserToken()).update({
-                imageUrl: url,
-                storageUrl: fileSnapshot.metadata.fullPath
-            })
-        });
-    }).catch(function (error) {
-        console.error('There was an error uploading a file to Cloud Storage:', error);
-    });
+    let fileSnapshot = await storage.ref(filePath).put(file);
+    let url = await fileSnapshot.getDownloadURL();
+    return url;
 }
 
+async function getWasherFilterQuery(filters) {
+    var filteredWashers = [];
+    let washersArray = await db.collection('washers').get();
+    let firstQuery = true;
+
+    if (filters.commitment !== undefined) {
+        let filteredWashersWithCommitment = [];
+        await db.collection('washers').where(commitment, "<=", filters.commitment).get().forEach(doc => {
+            filteredWashersWithCommitment.push(doc);
+        });
+        filteredWashers = firstQuery ? filteredWashersWithCommitment : intersection(filteredWashers, filteredWashersWithCommitment);
+        firstQuery = false;
+    }
+    if (filters.rating !== undefined) {
+        let filteredWashersWithRating = [];
+        washersArray.forEach(doc => {
+            if (getRatingFromDoc(doc) >= filters.rating) {
+                filteredWashersWithRating.push(doc);
+            }
+        });
+        filteredWashers = firstQuery ? filteredWashersWithRating : intersection(filteredWashers, filteredWashersWithRating);
+        firstQuery = false;
+    }
+
+    if (filters.distance !== undefined) {
+        let filteredWashersWithDistance = [];
+        currentPoint = null; // fixme
+        washersArray.forEach(doc => {
+            if (getDistanceFromLatLonInKm(currentPoint, doc.location_cor) <= filters.distance) {
+                filteredWashersWithDistance.push(doc);
+            }
+        });
+        filteredWashers = firstQuery ? filteredWashersWithDistance : intersection(filteredWashers, filteredWashersWithDistance);
+        firstQuery = false;
+    }
+
+    if (filters.specialService !== undefined) {
+        let filteredWashersWithSpecialService = [];
+        washersArray.forEach(doc => {
+            // need to fix in case of adding multiple properties
+            if (doc.data().properties == filters.specialService[0]) {
+                filteredWashersWithSpecialService.push(doc);
+            }
+        });
+        filteredWashers = firstQuery ? filteredWashersWithSpecialService : intersection(filteredWashers, filteredWashersWithSpecialService);
+        firstQuery = false;
+    }
+
+    if (filters.openTime !== undefined) {
+        let filteredWashersWithOpenTime = [];
+        washersArray.forEach(doc => {
+            if (true) { // ????
+                filteredWashersWithOpenTime.push(doc);
+            }
+        });
+        filteredWashers = firstQuery ? filteredWashersWithOpenTime : intersection(filteredWashers, filteredWashersWithOpenTime);
+        firstQuery = false;
+    }
+
+    return filteredWashers;
+}
--- public/scripts/tools.js
+++ public/scripts/tools.js
@@ -2,6 +2,11 @@ function arrangeAddress(address) {
   return address.split(/[ ,]+/).join('+');
 }
 
+const intersection = (arrayA, arrayB) => {
+  arrayB = new Set(arrayB); // recycling variable
+  return [...new Set(arrayA)].filter(element => arrayB.has(element));
+};
+
 /**
  * the function takes latitude and longitude and 
  * @param {number} lat : latitude coordinate 
@@ -12,14 +17,16 @@ function reverseGeocoder(lat, lng) {
   var apiKey = '4c1b00bd6bfc462a8c289c794fd32112';
   var latitude = lat;
   var longitude = lng;
-  var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+  var api_url = 'https://api.opencagedata.com/geocode/v1/json';
+  let language = 'en';
 
   var request_url = api_url +
     '?' +
     'key=' + apiKey +
     '&q=' + encodeURIComponent(latitude + ',' + longitude) +
     '&pretty=1' +
-    '&no_annotations=1';
+    '&no_annotations=1' +
+    '&language=' + language;
 
   var data;
   var request = new XMLHttpRequest();
@@ -57,13 +64,17 @@ async function forwardGeocode(location_str) {
   var apiKey = '4c1b00bd6bfc462a8c289c794fd32112';
   var location = location_str;
   var api_url = 'https://api.opencagedata.com/geocode/v1/json';
+  let country = 'IL';
+  let language = 'en';
 
   var request_url = api_url +
     '?' +
     'key=' + apiKey +
     '&q=' + location +
     '&pretty=1' +
-    '&no_annotations=1';
+    '&no_annotations=1' +
+    '&countrycode=' + country +
+    '&language=' + language;
   // see full list of required and optional parameters:
   // https://opencagedata.com/api#forward
 
@@ -92,6 +103,24 @@ async function forwardGeocode(location_str) {
   };
 
   request.send(); // make the request
+}
+
+function getDistanceFromLatLonInKm(pointA, pointB) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(pointB.Latitude - pointA.Latitude);  // deg2rad below
+  var dLon = deg2rad(pointB.Longitude - pointA.Longitude); 
+  var a = 
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(pointA.Latitude)) * Math.cos(deg2rad(pointB.Latitude)) * 
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
 
 // /* Initialise Reverse Geocode API Client */
