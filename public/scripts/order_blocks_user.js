4de0231294e7aa78a8f0978d51fed8e83ee22266
nadav <nadavyoch@gmail.com>
2021-07-17 13:37:36 +0300

    changes in map filter

--- public/scripts/order_blocks_user.js
+++ public/scripts/order_blocks_user.js
@@ -100,7 +100,8 @@ function off() {
 
 async function load_quick_welcome_page() {
     console.log(" Here is a big bug but no time now will do tomooroow")
-    var userID = sessionStorage.getItem("connected_userID");
+    // var userID = sessionStorage.getItem("connected_userID");
+    var userID = getUserToken();
     if (userID != null) {
         await insert_orders_blocks_of_user("welcome_orders_block", userID, "finished"); 
     }
