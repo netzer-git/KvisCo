8420b901adf28cd914a649588054bb75c14349b6
Netzer <Netzerep@gmail.com>
2021-07-19 10:13:47 +0300

    change getBetterCloserWashers to get filters

--- public/scripts/firestore_tools.js
+++ public/scripts/firestore_tools.js
@@ -563,30 +563,31 @@ function sortOrdersByCreatedAt(orderArray) {
  * @param {*} indicator 1-3, indicates the wanted filter
  * @returns array of washer as dictated by the control number
  */
-async function getBetterCloserWashers(indicator, currentPoint) {
+async function getBetterCloserWashers(indicator, filters) {
     let washerArray = []
     switch (indicator) {
         case 1:
             washerArray = await getWasherFilterQuery({
+                ... filters,
                 rating: 4.5,
             });
             break;
         case 2:
             washerArray = await getWasherFilterQuery({
+                ... filters,
                 rating: 3,
                 distance: 3,
-                current_cor: currentPoint,
             });
             break;
         case 3:
             washerArray = await getWasherFilterQuery({
+                ... filters,
                 distance: 1.5,
-                current_cor: currentPoint,
             });
             break;
     }
     console.log(washerArray);
-    return sortWashersByDistance(washerArray, currentPoint);
+    return sortWashersByDistance(washerArray, filters.currentPoint);
 }
 
 async function getButtonAccordingToWasherStatus() {
