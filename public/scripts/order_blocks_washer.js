b0e07fc3834d45c83df257058cb184d4bf281b15
nadav <nadavyoch@gmail.com>
2021-07-19 11:55:00 +0300

    map filter, order blocks, place order, search bar

--- public/scripts/order_blocks_washer.js
+++ public/scripts/order_blocks_washer.js
@@ -38,7 +38,7 @@ async function get_order_block_of_washer(order_doc) {
             break;
     }
     // Date and Price blocks 
-    var date = new Date(order_doc.data().due_to*1000);
+    var date = new Date(order_doc.data().due_to);
     var formattedTime = date.getDate() + '/' + (date.getMonth()+1);
     if (date.getMinutes().toString().length <= 1) {
         var minutes = "0" +date.getMinutes();
